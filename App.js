b00db071c1af251b8e6ad0b2fee5838a07dbacf5
Kshitij Tagra <130739500+kshitij-tagra@users.noreply.github.com>
2024-03-06 14:04:22 -0700

    Update App.js

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { SafeAreaView } from "react-native";
 import ToDoList from "./components/ToDoList";
 import ToDoForm from "./components/ToDoForm";
 
 export default function App() {
+  const [tasks, setTasks] = useState(["Do Laundry", "Go to gym", "Walk dog"]);
+
   return (
     <SafeAreaView>
-      <ToDoList />
+      <ToDoList tasks={tasks} />
       <ToDoForm />
     </SafeAreaView>
   );

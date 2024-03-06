abe6111427b96b9a8803ad92bfec3e5730019e48
Kshitij Tagra <130739500+kshitij-tagra@users.noreply.github.com>
2024-03-06 14:04:57 -0700

    Update ToDoList.js

--- components/ToDoList.js
+++ components/ToDoList.js
@@ -1,24 +1,18 @@
 import React from "react";
 import { ScrollView, View, Text, Pressable, StyleSheet } from "react-native";
 
-export default function ToDoList() {
+export default function ToDoList({ tasks }) {
   return (
     <ScrollView>
-      <Pressable>
-        <View style={[styles.task, styles.completed]}>
-          <Text style={styles.taskText}>Do laundry</Text>
-        </View>
-      </Pressable>
-      <Pressable>
-        <View style={[styles.task]}>
-          <Text style={styles.taskText}>Go to gym</Text>
-        </View>
-      </Pressable>
-      <Pressable>
-        <View style={[styles.task, styles.completed]}>
-          <Text style={styles.taskText}>Walk dog</Text>
-        </View>
-      </Pressable>
+      {tasks.map((item) => {
+        return (
+          <Pressable>
+            <View style={[styles.task]}>
+              <Text style={styles.taskText}>{item}</Text>
+            </View>
+          </Pressable>
+        );
+      })}
     </ScrollView>
   );
 }

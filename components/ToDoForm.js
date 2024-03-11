6ae3acf75cecf97d99ce354d85cc5d47ead0199e
Kshitij Tagra <130739500+kshitij-tagra@users.noreply.github.com>
2024-03-10 19:25:06 -0600

    Update ToDoForm.js (Lab Assignment 4)

--- components/ToDoForm.js
+++ components/ToDoForm.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, TextInput, Button, StyleSheet } from "react-native";
 
-export default function ToDoForm() {
+export default function ToDoForm({ addTask }) {
+  const [taskText, setTaskText] = useState("");
+
   return (
     <View style={styles.form}>
-      <TextInput style={styles.input} placeholder="Add a new task..." />
-      <Button title="Add" />
+      <TextInput
+        style={styles.input}
+        placeholder="Add a new task..."
+        onChangeText={(text) => setTaskText(text)}
+        value={taskText}
+      />
+      <Button title="Add" onPress={() => addTask(taskText)} />
     </View>
   );
 }
